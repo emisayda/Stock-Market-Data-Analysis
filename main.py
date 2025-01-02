7cf20fd5824067d5b2d5ab4c4fb2ef64194373c4
unknown <your_email@example.com>
2025-01-02 22:10:22 +0300

    day2

--- main.py
+++ main.py
@@ -8,19 +8,11 @@ df = pd.read_csv("data.csv")
 
 df["Date"] = df["Date"].replace("-","/", regex=True)
 df["Date"] = pd.to_datetime(df["Date"], errors='coerce')
-df.set_index("Date", inplace=True)
+#df.set_index("Date", inplace=True)
 
-df["Close/Last"] = df["Close/Last"].replace("$","", regex=True)
-#df["Close/Last"] = df["Close/Last"].replace(".","", regex=False)
-#df["Close/Last"] = df["Close/Last"].str.strip()
-#df["Close/Last"] = df["Close/Last"].replace('', '0')
-df["Close/Last"] = pd.to_numeric(df["Close/Last"], errors='coerce')
-df["Close/Last"] = df["Close/Last"].astype(float)
-df["Close/Last"] = df["Close/Last"].fillna(0)  # Replace NaN with 0
+df["Close/Last"] = df["Close/Last"].str.replace("$","").astype(float)
 
-df["Weekly_Roll"] =df["Close/Last"].rolling(window=7).mean()
-
-AAPL= df[df["Company"]== "AAPL"]
+AAPL = df[df["Company"] =="AAPL"]
 SBUX = df[df["Company"]=='SBUX']
 MSFT = df[df["Company"]=='MSFT']
 CSCO = df[df["Company"]=='CSCO']
@@ -30,16 +22,40 @@ AMZN = df[df["Company"]=='AMZN']
 TSLA = df[df["Company"]=='TSLA'] 
 AMD  = df[df["Company"]=='AMD']
 NFLX = df[df["Company"]=='NFLX']
-plt.figure(figsize=(10,6))
-plt.plot(AAPL.index, AAPL["Weekly_Roll"])
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-plt.xticks(rotation=45)
 
-print(AAPL["Weekly_Roll"].head(10))
+fig, axes = plt.subplots(5,2, figsize= (15,20))
+axes = axes.flatten()
 
 
-plt.xlabel("date")
-plt.ylabel("closing prices")
-plt.title("closing prices of AAPL")
+axes[0].plot(AAPL['Date'], AAPL['Close/Last'])
+axes[0].set_title('Class AAPL')
+
+axes[1].plot(SBUX['Date'], SBUX['Close/Last'])
+axes[1].set_title('SBUX')
+
+axes[2].plot(MSFT['Date'], MSFT['Close/Last'])
+axes[2].set_title('MSFT')
+
+axes[3].plot(CSCO['Date'], CSCO['Close/Last'])
+axes[3].set_title('CSCO')
+
+axes[4].plot(QCOM['Date'], QCOM['Close/Last'])
+axes[4].set_title('QCOM')
+
+axes[5].plot(META['Date'], META['Close/Last'])
+axes[5].set_title('META')
+
+axes[6].plot(AMZN['Date'], AMZN['Close/Last'])
+axes[6].set_title('AMZN')
+
+axes[7].plot(TSLA['Date'], TSLA['Close/Last'])
+axes[7].set_title('TSLA')
+
+axes[8].plot(AMD['Date'], AMD['Close/Last'])
+axes[8].set_title('AMD')
+
+axes[9].plot(NFLX['Date'], NFLX['Close/Last'])
+axes[9].set_title('NFLX')
+
+plt.tight_layout()
 plt.show()
